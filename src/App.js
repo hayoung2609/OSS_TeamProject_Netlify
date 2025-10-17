4b7ac92559f767874f80441714e23c2a63669313
hayoung <kanghayoung527@gmail.com>
2025-10-17 10:03:14 +0900

    mo app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './router/Router';
 import './App.css';
+import Header from './component/header.js';
+import Footer from './component/footer.js';
 
 function App() {
   return (
